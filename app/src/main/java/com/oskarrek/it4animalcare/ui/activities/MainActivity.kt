c18b1d08bd6297bf73bb188d08e4e81cd2febb81
Oskar Rek <oskar.rek@leiplocal.local>
2019-12-30 22:44:43 +0100

    Folders sturcture.

--- app/src/main/java/com/oskarrek/it4animalcare/ui/activities/MainActivity.kt
+++ app/src/main/java/com/oskarrek/it4animalcare/ui/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.oskarrek.it4animalcare
+package com.oskarrek.it4animalcare.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.oskarrek.it4animalcare.R
 
 class MainActivity : AppCompatActivity() {
 
